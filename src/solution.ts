0db9be0a4e621c6c5b53dc8a772a20246298d9f4
Asif Altair <asifaltair74@gmail.com>
2025-11-17 00:06:59 +0600

    solved problem no 4

--- src/solution.ts
+++ src/solution.ts
@@ -18,11 +18,11 @@ const formatValue = (value: string | number | boolean) => {
 
 
 // problem 2:
-const getLength  = (value : string | any[]): number => {
-    if (typeof value === "string"){
+const getLength = (value: string | any[]): number => {
+    if (typeof value === "string") {
         const length = value.length;
         return length;
-    }else if(Array.isArray(value)){
+    } else if (Array.isArray(value)) {
         return value.length;
     }
     return 0
@@ -33,13 +33,24 @@ class Person {
     name: string;
     age: number;
 
-    constructor(name: string, age: number){
+    constructor(name: string, age: number) {
         this.name = name;
         this.age = age;
     }
 
-    getDetails(): string{
+    getDetails(): string {
         return `Name: ${this.name} , Age: ${this.age}`
     }
 }
+
+// problem 4:
+type Item = {
+    title: string;
+    rating: number
+};
+
+const filterByRating = (items: Item[]): Item[] => {
+    const filteredItem = items.filter(item => item.rating >= 4);
+    return filteredItem;
+};
 
