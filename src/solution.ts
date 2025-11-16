89f193aa42f48cd22ac957a474de3b0a2b676285
Asif Altair <asifaltair74@gmail.com>
2025-11-16 23:31:07 +0600

    complete problem 1

--- src/solution.ts
+++ src/solution.ts
@@ -1,3 +1,20 @@
-let age : number = 22;
-age = 25
-console.log(age)
+// TODO: REMOVE THE COMMENT
+// * problem 1:
+const formatValue = (value: string | number | boolean) => {
+    if (typeof value === "string") {
+        const uppercase = value.toUpperCase();
+        return uppercase
+    } else if (typeof value === "number") {
+        const multiple = value * 10;
+        return multiple;
+    } else if (typeof value === "boolean") {
+        if (value) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+};
+
+
+// problem 2:
